7c42191ec07a3559b473f45c5986b4ffb2809926
gleoni <gleoni@techinsights.com>
2022-06-24 04:45:43 -0300

    make it better

---
+++ Php/php18/database.php
@@ -0,0 +1,47 @@
+<?php 
+    $dbname="bv1npn2veaacv2l7ghih"; 
+    $host="bv1npn2veaacv2l7ghih-mysql.services.clever-cloud.com"; 
+    $user ="u6uxnal5anrjzncq"; 
+    $password = "mnNP6kMFSJZGKiXvgWMD"; 
+    $respuesta_estado = "resp";
+    $proyectos=[];
+
+    try { 
+        $dsn = "mysql:host=$host;dbname=$dbname";
+        $dbh = new PDO($dsn, $user, $password); /*Database Handle*/ 
+        $respuesta_estado = $respuesta_estado . "\nconexion exitosa"; 
+        echo ("exito");
+    } catch (PDOException $e) { 
+        $respuesta_estado = $respuesta_estado . "\n" . $e->getMessage(); 
+        echo ("falló");
+    }
+
+    $sql="select * from proyectos " ;
+
+    $stmt = $dbh->prepare($sql);
+
+    $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    
+    $stmt->execute();
+
+    While($fila = $stmt->fetch()) {
+        $objProyecto = new stdClass();
+        $objProyecto->registro=$fila['registro'];
+        $objProyecto->proyecto=$fila['proyecto'];
+        $objProyecto->referente=$fila['referente'];
+        $objProyecto->pais=$fila['pais'];
+        $objProyecto->inicio=$fila['inicio'];
+        $objProyecto->ingresos=$fila['ingresos'];
+        array_push($proyectos,$objProyecto);
+    }
+
+    $objProyectos = new stdClass(); 
+    $objProyectos->proyectos=$proyectos; 
+    $objProyectos->cuenta=count($proyectos);
+
+    $salidaJson = json_encode($objProyectos);
+
+    $dbh = null;
+
+    echo $salidaJson;
+?>
---
+++ Php/php18/script.js
@@ -0,0 +1,90 @@
+const 
+    $registro = document.querySelector("#registro"),
+    $proyecto = document.querySelector("#proyecto"),
+    $referente = document.querySelector("#referente"),
+    $pais = document.querySelector("#pais"),
+    $inicio = document.querySelector("#inicio"),
+    $ingresos = document.querySelector("#ingresos"),
+    $orden = document.querySelector("#orden")
+;
+    
+function vaciarTabla() {
+    while($tabla.childNodes.length > 0) {
+        $tabla.removeChild($tabla.childNodes[0]);
+    }
+}
+
+function cambiarOrden(e) {
+    $orden.value = e;
+}
+
+function traerJson() {
+    let datos = new FormData;
+    datos.append("registro", $registro.value);
+    datos.append("proyecto", $proyecto.value);
+    datos.append("referente", $referente.value);
+    datos.append("pais", $pais.value);
+    datos.append("inicio", $inicio.value);
+    datos.append("ingresos", $ingresos.value);
+    datos.append("orden", $orden.value);
+    
+    fetch("database.php", {
+        method: 'POST',
+        body: datos
+    }) 
+    .then(res => res.json() )
+    .then(json => armarTabla(json))
+    .catch(error => console.error('Error: ', error))
+    .finally(()=> {
+        setTimeout(()=>{},1500);
+    })
+}
+
+const $tabla = document.querySelector("#tabla");
+
+function armarTabla(json) {
+    vaciarTabla();
+    json.agentes.forEach(e => {
+        let tr = document.createElement("tr"),
+
+        tdRegistro = document.createElement("td"),
+        tdProyecto = document.createElement("td"),
+        tdReferente = document.createElement("td"),
+        tdPais = document.createElement("td"),
+        tdInicio = document.createElement("td"),
+        tdIngresos = document.createElement("td");
+
+        tdRegistro.innerHTML = e.registro;
+        tr.appendChild(tdRegistro);
+
+        tdProyecto.innerHTML = e.proyecto;
+        tr.appendChild(tdProyecto);
+
+        tdReferente.innerHTML = e.referente;
+        tr.appendChild(tdReferente);
+
+        tdPais.innerHTML = e.pais;
+        tr.appendChild(tdPais);
+
+        tdInicio.innerHTML = e.inicio;
+        tr.appendChild(tdInicio);
+
+        tdIngresos.innerHTML = e.ingresos;
+        tr.appendChild(tdIngresos);
+
+        $tabla.appendChild(tr);
+    });
+
+    document.getElementById("nRegistro").innerHTML = `Numero de registros: ${json.cantidad}`;
+}
+
+
+document.getElementById("cargar").addEventListener("click", ()=> traerJson());
+document.getElementById("limpiar").addEventListener("click", ()=> vaciarTabla());
+document.getElementById("registro").addEventListener("click", ()=> cambiarOrden("registro"));
+document.getElementById("proyecto").addEventListener("click", ()=> cambiarOrden("proyecto"));
+document.getElementById("referente").addEventListener("click", ()=> cambiarOrden("referente"));
+document.getElementById("pais").addEventListener("click", ()=> cambiarOrden("pais"));
+document.getElementById("inicio").addEventListener("click", ()=> cambiarOrden("inicio"));
+document.getElementById("ingresos").addEventListener("click", ()=> cambiarOrden("ingresos"));
+
